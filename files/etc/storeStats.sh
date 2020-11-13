bb109bf7c0e2c2493ad048efe86e87be76cede43
Christopher Ng <facboy@gmail.com>
2020-11-13 08:51:25 +0000

    Update to version R7800-master-r14887-a47279154e-20201110-1759

--- files/etc/storeStats.sh
+++ files/etc/storeStats.sh
@@ -3,4 +3,5 @@
 cd /tmp/rrd/$(uname -n)
 logger -t "LuCI statistics" collectd stopped, create backup archive
 tar c -zvf /etc/backup.stats/stats.tar.gz *
+cp /etc/backup.stats/stats.tar.gz /etc/backup.stats/stats-$(date +%Y%m%dT%H%M).tar.gz
 /etc/init.d/collectd start
