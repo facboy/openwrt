7cfe437704b2a3d11f63ce263e9bb937892252e0
Christopher Ng <facboy@gmail.com>
2021-01-07 13:21:40 +0000

    Update to version R7800-master-r15437-b4c0d377f6-20210106-0927

--- files/etc/saveHNsettings.sh
+++ files/etc/saveHNsettings.sh
@@ -3,7 +3,7 @@ cd /etc
 tar -czv -f /tmp/HNsettings \
   config/network config/wireless config/firewall config/dhcp config/sqm \
   config/luci_statistics config/bcp38 \
-  dropbear/authorized_keys adblock/adblock.whitelist
+  dropbear/authorized_keys adblock/adblock.whitelist crontabs/root
 if [ "$?" -eq 0 ] ; then
   ccencrypt -vf /tmp/HNsettings
   if [ "$?" -eq 0 ] ; then
