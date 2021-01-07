82ca2ade5e2ac7829e2723f60ccb6b923285826c
Christopher Ng <facboy@gmail.com>
2021-01-07 11:19:37 +0000

    Update to version R7800-master-r15437-b4c0d377f6-20210106-0927

--- hnscripts/parallelcompile.sh
+++ hnscripts/parallelcompile.sh
@@ -6,7 +6,7 @@ echo "...create version info file..."
 hnscripts/timestampVersion.sh
 echo "...make world..."
 mkdir -p logs
-make -j 5 V=s 2>&1 | tee logs/build.log | grep -i -E "^make.*(error|[12345]...Entering dir)"
+make -j 5 V=s 2>&1 | tee logs/build.log | grep -i -E "^make.*(error|[12345]...Entering dir)|^warning"
 [ ${PIPESTATUS[0]} -ne 0 ] && exit 1
 echo -n "...create build info: "
 hnscripts/createbuildinfo.sh
